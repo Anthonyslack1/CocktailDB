f37f99b31e5f6ecaf6fe766a54f539b6bcad5fca
anthony.slack <anthonyslack1@gmail.com>
2022-01-22 18:03:18 -0500

    Adding lazy loading and angular material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./home/home-routing.module').then(m => m.HomeRoutingModule)
+  },
+  {
+    path: 'cocktails',
+    loadChildren: () => import('./cocktails/cocktails-routing.module').then(m => m.CocktailsRoutingModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
